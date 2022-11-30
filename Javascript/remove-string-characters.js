1accd2c49cee70d72e90622b90681ad54d9c7921
Alex Mq <alemarqred@gmail.com>
2022-11-30 09:56:34 +0100

    quitaracentos

--- Javascript/remove-string-characters.js
+++ Javascript/remove-string-characters.js
@@ -14,3 +14,25 @@ function facetFiltersChilds() {
 		});
 	});
 }
+
+// Remove accents
+
+function quitarAcentos(cadena) {
+	const acentos = {
+		á: 'a',
+		é: 'e',
+		í: 'i',
+		ó: 'o',
+		ú: 'u',
+		Á: 'A',
+		É: 'E',
+		Í: 'I',
+		Ó: 'O',
+		Ú: 'U',
+	};
+	return cadena
+		.split('')
+		.map((letra) => acentos[letra] || letra)
+		.join('')
+		.toString();
+}
